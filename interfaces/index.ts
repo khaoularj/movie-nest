d2c7dea84c504954712764523e34b9b9f4c2a8b9
khaoularj <khaoula.raja1@gmail.com>
2025-03-10 01:12:19 +0000

    adding interfaces

---
+++ interfaces/index.ts
@@ -0,0 +1,20 @@
+// Define the type for a movie
+export interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    description: string;
+    genre: string;
+    rating: number;
+}
+
+export interface MovieCardProps {
+    movie: {
+        id: number;
+        title: string;
+        poster_path: string;
+        description: string;
+        genre: string;
+        rating: number;
+    };
+}
